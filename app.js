b1e4ce3cf82fecdc9097e1bd0afde77c6932196d
oshanedesign <oshane.design@gmail.com>
2022-07-02 16:48:23 -0500

    edit click btn

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
 //GET 
 const getAdviceNumber = document.querySelector('#adviceNumber');
 const adviceResultsDiv = document.querySelector('#adviceResults');
+const getDataBtn = document.querySelector('#getDataBtn')
+
 const getAPI = async () => {
-    const res = await axios.get('https://api.adviceslip.com/advice');
- console.log(res.data.slip.advice);
+  const res = await axios.get('https://api.adviceslip.com/advice');
+//  console.log(res.data.slip.advice);
  
 
-
 //  generate unique id number
-const header = document.createElement('h1');
-header.className = 'title'
-header.append(` ADVICE # ${res.data.slip.id}`);
-getAdviceNumber.append(header);
+  const header = document.createElement('h1');
+  header.className = 'title'
+  header.append(` ADVICE # ${res.data.slip.id}`);
+  getAdviceNumber.append(header);
 
 //generate unique advice
-const para = document.createElement('p');
-para.className = 'para';
-para.append(`"${res.data.slip.advice}"`)
-adviceResultsDiv.append(para)
-}
+  const para = document.createElement('p');
+  para.className = 'para';
+  para.append(`"${res.data.slip.advice}"`)
+  adviceResultsDiv.append(para)
 
+
+  //click button to get unique id and advice
+}
 
 getAPI();
 
 
-
-
-
-// const getDataBtnDiv = document.querySelector('#getDataBtn');
-
-
-//
