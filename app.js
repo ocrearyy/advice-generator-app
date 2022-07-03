70e73d58f05079d56495c241672add1da09df85f
oshanedesign <oshane.design@gmail.com>
2022-07-03 09:39:42 -0500

    adddata on btn click

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 //GET 
 const getAdviceNumber = document.querySelector('#adviceNumber');
 const adviceResultsDiv = document.querySelector('#adviceResults');
-const getDataBtn = document.querySelector('#getDataBtn')
-
-const getAPI = async () => {
-  const res = await axios.get('https://api.adviceslip.com/advice');
-//  console.log(res.data.slip.advice);
- 
-
-//  generate unique id number
-  const header = document.createElement('h1');
-  header.className = 'title'
-  header.append(` ADVICE # ${res.data.slip.id}`);
-  getAdviceNumber.append(header);
-
-//generate unique advice
-  const para = document.createElement('p');
-  para.className = 'para';
-  para.append(`"${res.data.slip.advice}"`)
-  adviceResultsDiv.append(para)
+const diceBtn = document.querySelector('#getDataBtn')
 
 
-  //click button to get unique id and advice
-}
-
-getAPI();
+diceBtn.addEventListener('click', () => {
+  const getAPI = async () => {
+  // Call API  
+    const res = await axios.get('https://api.adviceslip.com/advice');   
+  
+  // generate unique id number
+    const header = document.createElement('h1');
+    header.className = 'title'
+    header.append(` ADVICE # ${res.data.slip.id}`);
+    getAdviceNumber.append(header);
+  
+  // generate unique advice
+    const para = document.createElement('p');
+    para.className = 'para';
+    para.append(`"${res.data.slip.advice}"`)
+    adviceResultsDiv.append(para)
+  }
+})
+// getAPI();
 
 
