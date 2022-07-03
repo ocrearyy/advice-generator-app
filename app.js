23ce481a7d212a1a892add8cdd71740565b15847
Prantik <prantiknoor@gmail.com>
2022-07-03 22:38:19 +0600

    fetch error solved & card centerd

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 // GET
-const getAdviceNumber = document.querySelector('#adviceNumber');
-const adviceResultsDiv = document.querySelector('#adviceResults');
-const diceBtn = document.querySelector('#getDataBtn');
+const getAdviceNumber = document.querySelector("#adviceNumber");
+const adviceResultsDiv = document.querySelector("#adviceResults");
+const diceBtn = document.querySelector("#getDataBtn");
 
+const URL = "https://api.adviceslip.com/advice";
 
-
-diceBtn.addEventListener('click', () => {
+diceBtn.addEventListener("click", () => {
   const getAPI = async () => {
-  // Call API
-    const res = await axios.get('https://api.adviceslip.com/advice');
+    // Call API
+    const res = fetchQoute();
 
     // generate unique id number
-    const header = document.createElement('h1');
-    header.className = 'title';
+    const header = document.createElement("h1");
+    header.className = "title";
     header.append(` ADVICE # ${res.data.slip.id}`);
+    while (getAdviceNumber.childElementCount > 0) {
+      getAdviceNumber.firstChild.remove();
+    }
     getAdviceNumber.append(header);
 
     // generate unique advice
-    const para = document.createElement('p');
-    para.className = 'para';
+    const para = document.createElement("p");
+    para.className = "para";
     para.append(`"${res.data.slip.advice}"`);
+    while (adviceResultsDiv.childElementCount > 0) {
+      adviceResultsDiv.firstChild.remove();
+    }
     adviceResultsDiv.append(para);
   };
   getAPI();
 });
-// getAPI();
+
+function fetchQoute() {
+  function httpGet(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+  }
+
+  let response = httpGet(URL);
+  return { data: JSON.parse(response) };
+}
