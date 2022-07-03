e077fa68f8c187380a64a5b3a547aeeeca54c2f7
oshanedesign <oshane.design@gmail.com>
2022-07-03 09:51:48 -0500

    fix click btn

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-//GET 
+// GET
 const getAdviceNumber = document.querySelector('#adviceNumber');
 const adviceResultsDiv = document.querySelector('#adviceResults');
-const diceBtn = document.querySelector('#getDataBtn')
-
+const diceBtn = document.querySelector('#getDataBtn');
 
 diceBtn.addEventListener('click', () => {
   const getAPI = async () => {
-  // Call API  
-    const res = await axios.get('https://api.adviceslip.com/advice');   
-  
-  // generate unique id number
+  // Call API
+    const res = await axios.get('https://api.adviceslip.com/advice');
+
+    // generate unique id number
     const header = document.createElement('h1');
-    header.className = 'title'
+    header.className = 'title';
     header.append(` ADVICE # ${res.data.slip.id}`);
     getAdviceNumber.append(header);
-  
-  // generate unique advice
+
+    // generate unique advice
     const para = document.createElement('p');
     para.className = 'para';
-    para.append(`"${res.data.slip.advice}"`)
-    adviceResultsDiv.append(para)
-  }
-})
+    para.append(`"${res.data.slip.advice}"`);
+    adviceResultsDiv.append(para);
+  };
+  getAPI();
+});
 // getAPI();
-
-
