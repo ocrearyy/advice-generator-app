a9fdb422de4b4928bdc17545bc5f0bba7dfbffce
oshanedesign <oshane.design@gmail.com>
2022-07-03 12:38:03 -0500

    implement full functions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const getAdviceNumber = document.querySelector('#adviceNumber');
 const adviceResultsDiv = document.querySelector('#adviceResults');
 const diceBtn = document.querySelector('#getDataBtn');
+const staticText = document.querySelector('#static');
 
 
 
@@ -10,18 +11,28 @@ diceBtn.addEventListener('click', () => {
   // Call API
     const res = await axios.get('https://api.adviceslip.com/advice');
 
+    //remove static text 
+    staticText.remove();
+
     // generate unique id number
     const header = document.createElement('h1');
     header.className = 'title';
     header.append(` ADVICE # ${res.data.slip.id}`);
+    while (getAdviceNumber.childElementCount > 0) {
+      getAdviceNumber.firstChild.remove();
+    }
     getAdviceNumber.append(header);
 
     // generate unique advice
     const para = document.createElement('p');
     para.className = 'para';
     para.append(`"${res.data.slip.advice}"`);
+    while (adviceResultsDiv.childElementCount > 0) {
+      adviceResultsDiv.firstChild.remove();
+    }
     adviceResultsDiv.append(para);
   };
   getAPI();
+  
 });
 // getAPI();
